b05fac81719949cf78b07f5daf73b3d5f0e3ccb1
aa-ag <aaron.aguerrevere@gmail.com>
2021-02-04 18:29:00 -0600

    edited docstrings

--- script.py
+++ script.py
@@ -20,33 +20,37 @@ def extract_text_from_pdf():
             cd.write(page.extractText())
 
 
-# def read_csv_and_create_wordcloud():
-#     df = pd.read_csv(r'Youtube03-LMFAO.csv', encoding="latin-1")
+def read_csv_and_create_wordcloud():
+    '''
+      opens csv as dataframe, reads thru it and creates string with text,
+      ignoring 192 stopwords. Then creates wordcloud with that string.
+    '''
+    df = pd.read_csv(r'Youtube03-LMFAO.csv', encoding="latin-1")
 
-#     comments = ''
-#     sw = set(STOPWORDS)
+    comments = ''
+    sw = set(STOPWORDS)
 
-#     for i in df.CONTENT:
-#         i = str(i)
+    for i in df.CONTENT:
+        i = str(i)
 
-#         tokens = i.split()
+        tokens = i.split()
 
-#         for j in range(len(tokens)):
-#             tokens[j] = tokens[j].lower()
+        for j in range(len(tokens)):
+            tokens[j] = tokens[j].lower()
 
-#         comments += ' '.join(tokens) + ' '
+        comments += ' '.join(tokens) + ' '
 
-#     wc = WordCloud(width=800, height=800, background_color='grey',
-#                    stopwords=sw, min_font_size=10).generate(comments)
+    wc = WordCloud(width=800, height=800, background_color='grey',
+                   stopwords=sw, min_font_size=10).generate(comments)
 
-#     plt.figure(figsize=(10, 10), facecolor=None)
-#     plt.imshow(wc)
-#     plt.axis('off')
-#     plt.tight_layout(pad=0)
-#     plt.show()
+    plt.figure(figsize=(10, 10), facecolor=None)
+    plt.imshow(wc)
+    plt.axis('off')
+    plt.tight_layout(pad=0)
+    plt.show()
 
 
 ###--- DRIVER CODE ---###
 if __name__ == '__main__':
-    # read_csv_and_create_wordcloud()
-    extract_text_from_pdf()
+    read_csv_and_create_wordcloud()
+    # extract_text_from_pdf()
