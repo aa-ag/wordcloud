b2cecd27cb0ae678975cdfb0f371136080c8981a
aa-ag <aaron.aguerrevere@gmail.com>
2021-02-05 19:01:40 -0600

    txt reader working

--- script.py
+++ script.py
@@ -25,29 +25,12 @@ def read_csv_and_create_wordcloud():
       opens csv as dataframe, reads thru it and creates string with text,
       ignoring 192 stopwords. Then creates wordcloud with that string.
     '''
-    df = pd.read_csv(r'Youtube03-LMFAO.csv', encoding="latin-1")
+    text = open('courtdecision.txt', 'r')
 
-    comments = ''
-    sw = set(STOPWORDS)
+    for line in text:
+        print(line)
 
-    for i in df.CONTENT:
-        i = str(i)
-
-        tokens = i.split()
-
-        for j in range(len(tokens)):
-            tokens[j] = tokens[j].lower()
-
-        comments += ' '.join(tokens) + ' '
-
-    wc = WordCloud(width=800, height=800, background_color='grey',
-                   stopwords=sw, min_font_size=10).generate(comments)
-
-    plt.figure(figsize=(10, 10), facecolor=None)
-    plt.imshow(wc)
-    plt.axis('off')
-    plt.tight_layout(pad=0)
-    plt.show()
+    text.close()
 
 
 ###--- DRIVER CODE ---###
